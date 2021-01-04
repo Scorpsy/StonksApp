400a32ef35a9df721ffda4d6454cda070a784ca2
Scorpsy <matthewraynard65@gmail.com>
2021-01-03 16:06:08 -0800

    setting up web page and dividing data

--- Data/DataHandling.py
+++ Data/DataHandling.py
@@ -14,21 +14,26 @@ def giveData(stockSymbol):
     today = datetime.datetime.today().isoformat()
     print('Today = ' + today)
 
+    month = '1m'
+
     tickerDF = tickerdata.history(period = '1m', start= '2020-12-1', end=today[:10])
     #pricelast = tickerDF['Close'].iloc[-1]
     #priceYest = tickerDF['Close'].iloc[-2]
     #print(investment + ' price last = ' + str(pricelast))
-
-    #print(tickerDF)
+    
     return tickerDF
 
-
+#Use Python3 for command prompt
+#iloc used to get range of data 
 def main():
 
     symbol = input("Enter Symbol: ")
-
     data = giveData(symbol)
-    print(data)
+
+    pricelast = data['Open'].iloc[1:5]
+
+    #print(data)
+    print(pricelast)
 
 if __name__ == '__main__':
     main()
